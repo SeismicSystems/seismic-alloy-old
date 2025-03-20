787d39cebe2c3b7f65c42ee93c9d70580f6f8e4f
phexyz <32248504+phexyz@users.noreply.github.com>
2025-03-20 13:02:10 -0400

    last commit

--- crates/provider/src/layers/seismic/http.rs
+++ crates/provider/src/layers/seismic/http.rs
@@ -108,10 +108,7 @@ mod tests {
         let plaintext = ContractTestContext::get_deploy_input_plaintext();
         let anvil = Anvil::new().spawn();
         let wallet = get_wallet(&anvil);
-        let provider = SeismicSignedProvider::new(
-            wallet.clone(),
-            reqwest::Url::parse("http://localhost:8545").unwrap(),
-        );
+        let provider = SeismicSignedProvider::new(wallet.clone(), anvil.endpoint_url());
 
         let tx = TransactionRequest::default().with_input(plaintext).with_kind(TxKind::Create);
 
@@ -125,8 +122,7 @@ mod tests {
         let plaintext = ContractTestContext::get_deploy_input_plaintext();
         let anvil = Anvil::new().spawn();
         let from = get_wallet(&anvil).default_signer().address();
-        let unsigned_provider =
-            SeismicUnsignedProvider::new(reqwest::Url::parse("http://localhost:8545").unwrap());
+        let unsigned_provider = SeismicUnsignedProvider::new(anvil.endpoint_url());
 
         let tx = TransactionRequest::default()
             .with_input(plaintext)
