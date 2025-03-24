e8c8de517dd7b67e7cef56e1524a96c9690cc4b9
phexyz <32248504+phexyz@users.noreply.github.com>
2025-03-24 13:37:04 -0400

    last commit (#65)

--- crates/provider/src/layers/seismic/http.rs
+++ crates/provider/src/layers/seismic/http.rs
@@ -1,8 +1,8 @@
 //! Seismic provider for HTTP requests
 use crate::{
     fillers::{
-        BlobGasFiller, CachedNonceManager, ChainIdFiller, FillProvider, GasFiller, JoinFill,
-        NonceFiller, RecommendedFillers, WalletFiller,
+        BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller,
+        RecommendedFillers, SimpleNonceManager, WalletFiller,
     },
     Identity, ProviderBuilder, RootProvider,
 };
@@ -17,7 +17,7 @@ pub type SeismicSignedProviderInner = SeismicProvider<
         JoinFill<
             JoinFill<
                 GasFiller,
-                JoinFill<BlobGasFiller, JoinFill<NonceFiller<CachedNonceManager>, ChainIdFiller>>,
+                JoinFill<BlobGasFiller, JoinFill<NonceFiller<SimpleNonceManager>, ChainIdFiller>>,
             >,
             WalletFiller<EthereumWallet>,
         >,
@@ -43,7 +43,7 @@ impl SeismicSignedProvider {
                 JoinFill::new(
                     BlobGasFiller,
                     JoinFill::new(
-                        NonceFiller::<CachedNonceManager>::default(),
+                        NonceFiller::<SimpleNonceManager>::default(),
                         ChainIdFiller::default(),
                     ),
                 ),
